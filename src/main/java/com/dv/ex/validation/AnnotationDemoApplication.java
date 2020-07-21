8020388c07bea1e2dd6df6e62409ecbf05525f1d
Devlin Verley <verley93@gmail.com>
2020-07-21 17:54:41 -0500

    Initial commit

---
+++ src/main/java/com/dv/ex/validation/AnnotationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dv.ex.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnotationDemoApplication.class, args);
+	}
+
+}
