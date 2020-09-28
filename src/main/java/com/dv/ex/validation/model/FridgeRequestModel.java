ad2e5cbae4cce0afa69e274d0a908e5c08ec3028
Devlin Verley <verley93@gmail.com>
2020-09-27 21:49:16 -0500

    Remove unused constructor

--- src/main/java/com/dv/ex/validation/model/FridgeRequestModel.java
+++ src/main/java/com/dv/ex/validation/model/FridgeRequestModel.java
@@ -1,7 +1,6 @@
 package com.dv.ex.validation.model;
 
 import com.dv.ex.validation.validator.FoodCategory;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,7 +8,6 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class FridgeRequestModel extends FoodRequestModel {
 
     @FoodCategory(allowed = {"dairy", "vegetables", "beer"})
