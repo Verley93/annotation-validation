38a8bec0ad227fc574eb30394a12c96d36893546
Devlin Verley <verley93@gmail.com>
2020-09-27 21:00:17 -0500

    Refactor test classes by controller method

--- src/test/java/com/dv/ex/validation/ValidationDemoApplicationTests.java
+++ src/test/java/com/dv/ex/validation/ValidationDemoApplicationTests.java
@@ -36,71 +36,76 @@ class ValidationDemoApplicationTests {
     @Nested
     class FridgeServiceTests {
 
-        @Test
-        void testAddFoodToFridgeV1_Success() throws Exception {
-            FridgeRequestModel fridgeRequest = new FridgeRequestModel();
-            fridgeRequest.setName("chicken");
-            fridgeRequest.setCategory("meats");
-            fridgeRequest.setQuantity(3);
-            fridgeRequest.setRefrigerated(true);
+        @Nested
+        class AddFoodToFridgeV1 {
 
-            mockMvc.perform(MockMvcRequestBuilders
-                    .post(URI_FRIDGE)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(fridgeRequest)))
-                    .andExpect(status().isOk());
-        }
+            @Test
+            void testSuccess() throws Exception {
+                FridgeRequestModel fridgeRequest = new FridgeRequestModel();
+                fridgeRequest.setName("chicken");
+                fridgeRequest.setCategory("meats");
+                fridgeRequest.setQuantity(3);
+                fridgeRequest.setRefrigerated(true);
 
-        @Test
-        void testAddFoodToFridgeV1_NegativeQuantity() throws Exception {
-            FridgeRequestModel fridgeRequest = new FridgeRequestModel();
-            fridgeRequest.setName("chicken");
-            fridgeRequest.setCategory("meats");
-            fridgeRequest.setQuantity(-3);
-            fridgeRequest.setRefrigerated(true);
+                mockMvc.perform(MockMvcRequestBuilders
+                        .post(URI_FRIDGE)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(fridgeRequest)))
+                        .andExpect(status().isOk());
+            }
 
-            mockMvc.perform(MockMvcRequestBuilders
-                    .post(URI_FRIDGE)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(fridgeRequest)))
-                    .andExpect(jsonPath("$.errorMessage")
-                            .value("Quantity must be positive"))
-                    .andExpect(status().isBadRequest());
-        }
+            @Test
+            void testNegativeQuantity() throws Exception {
+                FridgeRequestModel fridgeRequest = new FridgeRequestModel();
+                fridgeRequest.setName("chicken");
+                fridgeRequest.setCategory("meats");
+                fridgeRequest.setQuantity(-3);
+                fridgeRequest.setRefrigerated(true);
 
-        @Test
-        void testAddFoodToFridgeV1_HighQuantity() throws Exception {
-            FridgeRequestModel fridgeRequest = new FridgeRequestModel();
-            fridgeRequest.setName("chicken");
-            fridgeRequest.setCategory("meats");
-            fridgeRequest.setQuantity(50);
-            fridgeRequest.setRefrigerated(true);
+                mockMvc.perform(MockMvcRequestBuilders
+                        .post(URI_FRIDGE)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(fridgeRequest)))
+                        .andExpect(jsonPath("$.errorMessage")
+                                .value("Quantity must be positive"))
+                        .andExpect(status().isBadRequest());
+            }
 
-            mockMvc.perform(MockMvcRequestBuilders
-                    .post(URI_FRIDGE)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(fridgeRequest)))
-                    .andExpect(jsonPath("$.errorMessage")
-                            .value("Quantity must not exceed 25"))
-                    .andExpect(status().isBadRequest());
-        }
+            @Test
+            void testHighQuantity() throws Exception {
+                FridgeRequestModel fridgeRequest = new FridgeRequestModel();
+                fridgeRequest.setName("chicken");
+                fridgeRequest.setCategory("meats");
+                fridgeRequest.setQuantity(50);
+                fridgeRequest.setRefrigerated(true);
 
-        @Test
-        void testAddFoodToFridgeV1_InvalidCategory() throws Exception {
-            FridgeRequestModel fridgeRequest = new FridgeRequestModel();
-            fridgeRequest.setName("pretzels");
-            fridgeRequest.setCategory("snacks");
-            fridgeRequest.setQuantity(20);
-            fridgeRequest.setRefrigerated(false);
+                mockMvc.perform(MockMvcRequestBuilders
+                        .post(URI_FRIDGE)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(fridgeRequest)))
+                        .andExpect(jsonPath("$.errorMessage")
+                                .value("Quantity must not exceed 25"))
+                        .andExpect(status().isBadRequest());
+            }
 
-            mockMvc.perform(MockMvcRequestBuilders
-                    .post(URI_FRIDGE)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(fridgeRequest)))
-                    .andExpect(jsonPath(
-                            "$.errorMessage")
-                            .value("Category must be one of the following: [meats, vegetables, beer]"))
-                    .andExpect(status().isBadRequest());
+            @Test
+            void testInvalidCategory() throws Exception {
+                FridgeRequestModel fridgeRequest = new FridgeRequestModel();
+                fridgeRequest.setName("pretzels");
+                fridgeRequest.setCategory("snacks");
+                fridgeRequest.setQuantity(20);
+                fridgeRequest.setRefrigerated(false);
+
+                mockMvc.perform(MockMvcRequestBuilders
+                        .post(URI_FRIDGE)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(fridgeRequest)))
+                        .andExpect(jsonPath(
+                                "$.errorMessage")
+                                .value("Category must be one of the following: [meats, vegetables, beer]"))
+                        .andExpect(status().isBadRequest());
+            }
+
         }
 
     }
@@ -108,70 +113,75 @@ class ValidationDemoApplicationTests {
     @Nested
     class PantryServiceTests {
 
-        @Test
-        void testAddFoodToPantryV1_Success() throws Exception {
-            PantryRequestModel pantryRequest = new PantryRequestModel();
-            pantryRequest.setName("oatmeal");
-            pantryRequest.setCategory("grains");
-            pantryRequest.setQuantity(3);
-            pantryRequest.setRefrigerated(false);
+        @Nested
+        class AddFoodToPantryV1 {
 
-            mockMvc.perform(MockMvcRequestBuilders
-                    .post(URI_PANTRY)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(pantryRequest)))
-                    .andExpect(status().isOk());
-        }
+            @Test
+            void testSuccess() throws Exception {
+                PantryRequestModel pantryRequest = new PantryRequestModel();
+                pantryRequest.setName("oatmeal");
+                pantryRequest.setCategory("grains");
+                pantryRequest.setQuantity(3);
+                pantryRequest.setRefrigerated(false);
 
-        @Test
-        void testAddFoodToPantryV1_NegativeQuantity() throws Exception {
-            PantryRequestModel pantryRequest = new PantryRequestModel();
-            pantryRequest.setName("oatmeal");
-            pantryRequest.setCategory("grains");
-            pantryRequest.setQuantity(-3);
-            pantryRequest.setRefrigerated(false);
+                mockMvc.perform(MockMvcRequestBuilders
+                        .post(URI_PANTRY)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(pantryRequest)))
+                        .andExpect(status().isOk());
+            }
 
-            mockMvc.perform(MockMvcRequestBuilders
-                    .post(URI_PANTRY)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(pantryRequest)))
-                    .andExpect(jsonPath("$.errorMessage")
-                            .value("Quantity must be positive"))
-                    .andExpect(status().isBadRequest());
-        }
+            @Test
+            void testNegativeQuantity() throws Exception {
+                PantryRequestModel pantryRequest = new PantryRequestModel();
+                pantryRequest.setName("oatmeal");
+                pantryRequest.setCategory("grains");
+                pantryRequest.setQuantity(-3);
+                pantryRequest.setRefrigerated(false);
 
-        @Test
-        void testAddFoodToPantryV1_HighQuantity() throws Exception {
-            PantryRequestModel pantryRequest = new PantryRequestModel();
-            pantryRequest.setName("oatmeal");
-            pantryRequest.setCategory("grains");
-            pantryRequest.setQuantity(50);
-            pantryRequest.setRefrigerated(false);
+                mockMvc.perform(MockMvcRequestBuilders
+                        .post(URI_PANTRY)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(pantryRequest)))
+                        .andExpect(jsonPath("$.errorMessage")
+                                .value("Quantity must be positive"))
+                        .andExpect(status().isBadRequest());
+            }
 
-            mockMvc.perform(MockMvcRequestBuilders
-                    .post(URI_PANTRY)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(pantryRequest)))
-                    .andExpect(jsonPath("$.errorMessage")
-                            .value("Quantity must not exceed 25"))
-                    .andExpect(status().isBadRequest());
-        }
+            @Test
+            void testHighQuantity() throws Exception {
+                PantryRequestModel pantryRequest = new PantryRequestModel();
+                pantryRequest.setName("oatmeal");
+                pantryRequest.setCategory("grains");
+                pantryRequest.setQuantity(50);
+                pantryRequest.setRefrigerated(false);
 
-        @Test
-        void testAddFoodToPantryV1_InvalidCategory() throws Exception {
-            PantryRequestModel pantryRequest = new PantryRequestModel();
-            pantryRequest.setName("chicken");
-            pantryRequest.setCategory("meats");
-            pantryRequest.setQuantity(20);
-            pantryRequest.setRefrigerated(true);
+                mockMvc.perform(MockMvcRequestBuilders
+                        .post(URI_PANTRY)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(pantryRequest)))
+                        .andExpect(jsonPath("$.errorMessage")
+                                .value("Quantity must not exceed 25"))
+                        .andExpect(status().isBadRequest());
+            }
 
-            mockMvc.perform(MockMvcRequestBuilders
-                    .post(URI_PANTRY)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(pantryRequest)))
-                    .andExpect(jsonPath("$.errorMessage")
-                            .value("Category must be one of the following: [grains, canned, snacks]"))
-                    .andExpect(status().isBadRequest());
+            @Test
+            void testInvalidCategory() throws Exception {
+                PantryRequestModel pantryRequest = new PantryRequestModel();
+                pantryRequest.setName("chicken");
+                pantryRequest.setCategory("meats");
+                pantryRequest.setQuantity(20);
+                pantryRequest.setRefrigerated(true);
+
+                mockMvc.perform(MockMvcRequestBuilders
+                        .post(URI_PANTRY)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(pantryRequest)))
+                        .andExpect(jsonPath("$.errorMessage")
+                                .value("Category must be one of the following: [grains, canned, snacks]"))
+                        .andExpect(status().isBadRequest());
+            }
+
         }
 
     }
