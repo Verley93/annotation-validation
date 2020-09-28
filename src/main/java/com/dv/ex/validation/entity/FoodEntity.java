dc77d9e7ed7d21c0841dd79681e871cfda30d59f
Devlin Verley <verley93@gmail.com>
2020-09-27 19:05:18 -0500

    Fix id generation strategy

--- src/main/java/com/dv/ex/validation/entity/FoodEntity.java
+++ src/main/java/com/dv/ex/validation/entity/FoodEntity.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Getter
@@ -16,7 +17,7 @@ import javax.persistence.Id;
 public class FoodEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String category;
